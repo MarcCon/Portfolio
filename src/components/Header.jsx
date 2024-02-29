eccc1c63721c56335e0220291218591731484af4
Marc-Constantin Enke <marc.enke@aol.de>
2024-02-29 18:55:56 +0100

    Test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
               autoPlay
               muted
               loop
-              src="/public/VideoMe.mp4"
+              src="../public/VideoMe.mp4"
             ></video>
           </div>
         </div>
