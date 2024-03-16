0da77701496406b30928600707d719273c167a27
Marc-Constantin Enke <marc.enke@aol.de>
2024-03-16 15:46:53 +0100

    Footer Anpassung Responsiv

--- src/components/LegalFooter.jsx
+++ src/components/LegalFooter.jsx
@@ -5,23 +5,23 @@ import { IoMailOutline } from "react-icons/io5";
 
 function LegalFooter() {
   return (
-    <div className="mt-16 flex h-[100px] flex-col items-center justify-between px-16 lg:flex-row">
-      <div id="kontakt" className="relative mt-[-200px] block pt-[200px]"></div>
-
-      <div className="flex gap-4 lg:gap-16">
-        <div className="flex items-center gap-1 sm:gap-2">
-          <FaGithub className="text-base text-white sm:text-xl xl:text-2xl" />
-          <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
-            github.com/MarcCon
-          </p>
+    <div className="mt-16 flex h-[130px] flex-col items-center justify-center gap-4 sm:h-[100px] sm:px-16 md:flex-row md:justify-between">
+      <div className="flex flex-col gap-4 sm:flex-row lg:gap-16">
+        <div className="flex gap-4 md:gap-4 lg:gap-16 ">
+          <div className="flex items-center gap-1  sm:gap-1">
+            <FaGithub className="text-base text-white sm:text-xl xl:text-2xl" />
+            <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
+              github.com/MarcCon
+            </p>
+          </div>
+          <div className="flex items-center gap-1 sm:gap-1">
+            <IoMailOutline className="text-base text-white sm:text-xl xl:text-2xl" />
+            <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
+              kontakt@marcenke.de
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-1 sm:gap-2">
-          <IoMailOutline className="text-base text-white sm:text-xl xl:text-2xl" />
-          <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
-            kontakt@marcenke.de
-          </p>
-        </div>
-        <div className="flex items-center gap-1 sm:gap-2">
+        <div className="flex items-center justify-center gap-1 sm:gap-1">
           <CiLinkedin className="text-base text-white sm:text-xl xl:text-2xl" />
           <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
             LinkedIn
@@ -29,11 +29,11 @@ function LegalFooter() {
         </div>
       </div>
 
-      <div className="flex-0.3 mb-4 flex items-center justify-end gap-8">
-        <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
+      <div className="flex items-center gap-8">
+        <p className="font-inter text-xs text-gray-400 sm:text-base xl:text-lg">
           Impressum
         </p>
-        <p className="font-inter text-xs text-white sm:text-base xl:text-lg">
+        <p className="font-inter text-xs text-gray-400 sm:text-base xl:text-lg">
           Datenschutz
         </p>
       </div>
