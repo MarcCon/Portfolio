af24f626f7a2f6694a84a3467d6a7deab630b2d7
Marc-Constantin Enke <marc.enke@aol.de>
2024-03-17 02:33:42 +0100

    Header Anpassungen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,29 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div
-        id="about"
-        className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-16 px-4 pb-4 sm:px-16 sm:pb-48 lg:flex-row lg:px-6  "
-      >
-        <img
-          src="/aboutPic.jpg"
-          className="h-[300px] w-auto rounded-2xl xl:h-[400px]"
-        />
+      <div className="bg-darkblue pt-32">
+        <div
+          id="about"
+          className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-16 px-4 pb-4 sm:px-16 sm:pb-48 lg:flex-row lg:px-6  "
+        >
+          <img
+            src="/aboutPic.jpg"
+            className="h-[300px] w-auto rounded-2xl xl:h-[400px]"
+          />
 
-        <div>
-          <p className="text-center font-inter text-xl font-extrabold text-mypurple lg:text-left lg:text-2xl">
-            Über mich
-          </p>
-          <h2 className="pb-4 text-center font-inter text-2xl font-extrabold text-white lg:text-left lg:text-3xl">
-            React Front-End entwickler in Halle (Saale) 📍
-          </h2>
-          <p className="text-inter text-center text-lg font-thin text-white sm:text-xl lg:text-left lg:text-2xl">
-            Hi, mein Name ist Marc, ich entwickle mithilfe von React und
-            Tailwind funktionale Web-Anwendungen.
-          </p>
-          <p className="text-inter pb-8 pt-4 text-center text-lg font-thin text-white sm:text-xl lg:text-left lg:text-2xl xl:pt-8"></p>
+          <div>
+            <p className="text-center font-inter text-xl font-extrabold text-mypurple lg:text-left lg:text-2xl">
+              Über mich
+            </p>
+            <h2 className="pb-4 text-center font-inter text-2xl font-extrabold text-white lg:text-left lg:text-3xl">
+              React Front-End entwickler in Halle (Saale) 📍
+            </h2>
+            <p className="text-inter text-center text-lg font-thin text-white sm:text-xl lg:text-left lg:text-2xl">
+              Hi, mein Name ist Marc, ich entwickle mithilfe von React und
+              Tailwind funktionale Web-Anwendungen.
+            </p>
+            <p className="text-inter pb-8 pt-4 text-center text-lg font-thin text-white sm:text-xl lg:text-left lg:text-2xl xl:pt-8"></p>
+          </div>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 const Navbar = () => {
   return (
     <>
-      <nav className="fixed left-0 top-0 flex w-full items-center justify-end bg-darkblue p-8 text-base font-bold text-white shadow-md shadow-slate-800 sm:px-16 sm:text-xl">
+      <nav className="sticky left-0 top-0 flex w-full items-center justify-end bg-darkblue p-8 text-base font-bold text-white shadow-md shadow-slate-800 sm:px-16 sm:text-xl">
         <div className="mr-auto">
           <p className="w-[90px] font-bold transition duration-300 ease-in-out hover:text-mypurple">
             marc.dev
@@ -15,7 +15,7 @@ const Navbar = () => {
           {/* Verwende Link von react-scroll statt <a href="#"> */}
           <Link
             to="about"
-            offset={-290}
+            offset={-200}
             smooth={true}
             duration={500}
             className="cursor-pointer transition duration-300 ease-in-out hover:text-mypurple"
