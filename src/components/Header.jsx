0a3a96a83f73b8cfb674cb1d9e26fe0f38c87af4
Marc-Constantin Enke <marc.enke@aol.de>
2024-03-17 12:14:59 +0100

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,19 +60,17 @@ const Header = () => {
                   style={{ display: !isVideoLoaded ? "block" : "none" }}
                 />
               )}
-              <div className="h-22 w-22 flex items-center justify-center overflow-hidden rounded-full sm:h-52 sm:w-52 lg:h-64 lg:w-64">
-                <video
-                  className="min-h-full min-w-full"
-                  autoPlay
-                  muted
-                  loop
-                  playsInline
-                  src="/VideoMe.mp4"
-                  onLoadedData={() => setIsVideoLoaded(true)}
-                  onError={() => setVideoError(true)}
-                  style={{ display: isVideoLoaded ? "block" : "none" }}
-                ></video>
-              </div>
+              <video
+                className="h-24 min-h-full w-24 min-w-full rounded-full"
+                autoPlay
+                muted
+                loop
+                playsInline
+                src="/VideoMe.mp4"
+                onLoadedData={() => setIsVideoLoaded(true)}
+                onError={() => setVideoError(true)}
+                style={{ display: isVideoLoaded ? "block" : "none" }}
+              ></video>
             </div>
           </div>
           <div className="flex flex-row items-center justify-center gap-4 pt-8 sm:gap-8 sm:pt-16">
