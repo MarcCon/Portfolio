a95ce13a2a07dbca8079f267e0516a41c42cd4ce
Marc-Constantin Enke <marc.enke@aol.de>
2024-03-22 10:28:17 +0100

    kleine Anpassungen

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,7 +14,6 @@ function Portfolio() {
         <h2 className="mb-8 font-inter text-3xl font-extrabold text-white sm:text-4xl">
           Meine Zeitreise
         </h2>
-        {/* Stelle sicher, dass die Projekt-Komponenten den Platz vollständig ausfüllen */}
         <div className="w-full">
           <Project
             imageSrc="/landingPage.png"
@@ -22,20 +21,20 @@ function Portfolio() {
             title="Landing Page"
             technologies={["React", "Tailwind"]}
             description="Eine vollständig responsive Website für eine Fotografin"
-            githubLink="https://github.com/mein-projekt-repo"
+            githubLink="https://github.com/MarcCon/LandingPage-Fotografin"
             externalLink="https://larakuche-fotografie.de"
             imageOnLeft={true}
           />
 
           <Project
-            imageSrc="/WorldWise.png"
+            imageSrc="/NomadNexus.png"
             imageAlt="Bild meines Projekts"
             title="Reisetagebuch"
             technologies={["React", "CSS "]}
-            description="Eine vollständig responsive Website für eine Fotografin"
+            description="Eine Website zum tracken von Reisen."
             githubLink="https://github.com/mein-projekt-repo"
-            externalLink="https://larakuche-fotografie.de"
-            imageOnLeft={false} // Alterniere die Bildposition, wenn gewünscht
+            externalLink=""
+            imageOnLeft={false}
           />
         </div>
       </div>
